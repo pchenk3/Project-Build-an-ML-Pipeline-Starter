d0851c3e70a216e85debe4749d5a23cf6b69e3e4
pchenk3 <pchenk3@wgu.edu>
2025-09-21 15:29:02 -0400

    Step 4: fix errors

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -99,7 +99,7 @@ def go(args):
     mlflow.sklearn.save_model(
         sk_pipe,
         export_path,
-        serialization_format=mlflow.sklearn.SERIALIZATION_FORMAT_CLOUDPICKLE,
+   
         input_example = X_train.iloc[:5]
     )
     ######################################
