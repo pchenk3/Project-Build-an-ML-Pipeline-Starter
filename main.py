13c12426cc2d2744d29c3f9fc4e14d7ae3d3e115
pchenk3 <pchenk3@wgu.edu>
2025-09-22 16:21:11 -0400

    Step 4 complete

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def go(config: DictConfig):
             # NOTE: use the rf_config we just created as the rf_config parameter for the train_random_forest
             # step
 
-                _ = mlflow.run(
+            _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
                 entry_point="main",
                 parameters={
